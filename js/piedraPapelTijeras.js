99bf5f676c63f79d83023b58b837b2ba92a54c93
Jesús <jtipian@gmail.com>
2025-10-06 19:56:06 +0200

    Minijuego Piedra, Papel o Tijeras terminado: contador, reset y full responsive

--- js/piedraPapelTijeras.js
+++ js/piedraPapelTijeras.js
@@ -5,32 +5,37 @@
 let imgPuño1 = document.querySelector("#imgPuño1");
 let imgPuño2 = document.querySelector("#imgPuño2");
 
+//btn reset
+const btnReset = document.getElementById("btnResetGame");
+
+
 //capturamos IMG-BOTONES 
 let imgBtnPiedra = document.querySelector("#imgBtnPiedra");
 let imgBtnPapel = document.querySelector("#imgBtnPapel");
 let imgBtnTijeras = document.querySelector("#imgBtnTijeras");
 
+
 //capturamos texto 
 let resultado1 = document.querySelector(".resultado1")
 let puntosMaquina = document.querySelector(".puntosMaquina");
 let puntosJugador = document.querySelector(".puntosJugador");
-let contador = document.querySelector(".contador")
+let winTextJugador = document.querySelector(".winTextJugador");
+let loseTextJugador = document.querySelector(".loseTextJugador");
 let puntosGanar = 0;
 let puntosPerder = 0;
-
-
+//contadores de victoria 
+let winJugador = 0;
+let winMaquina = 0
 
 //evento si el jugador elige piedra 
-
 imgBtnPiedra.addEventListener("click", okPiedra);
 function okPiedra() {
 
-    let contador = 2;
 
-    //creamos un contador entre 0 y 2 (0 incluido)
+    //creamos un número entre 0 y 2 (0 incluido)
     let pptRandom = Math.floor(Math.random() * 3);
+
     console.log("Máquina eligió:", pptRandom);
-    console.log("Intentos", contador);
 
     let jugador = 0;
 
@@ -42,16 +47,21 @@ function okPiedra() {
         resetPuños()
 
 
+
     } else if (pptRandom === 1) {
         resultado1.textContent = "Perdiste 😭 ";
         puntosPerder++;
-        contador--;
 
         puntosMaquina.textContent = "Puntos de la Maquina: " + puntosPerder;
         imgPuño1.src = "../media/img/papel-btn.png"
         imgPuño1.style.animation = "none";
         imgPuño2.style.animation = "none";
+        winMaquina++;
+
         resetPuños();
+        intentos()
+
+
 
 
     } else {
@@ -62,42 +72,25 @@ function okPiedra() {
         imgPuño1.style.animation = "none";
         imgPuño1.style.transform = "scaleX(-1)";
         imgPuño2.style.animation = "none";
-        resetPuños()
+        winJugador++;
+        resetPuños();
+        intentos();
+
     }
 
-}
 
-
-
-
-
-
-
-
-function resetPuños() {
-
-    setTimeout(() => {
-        // volver a las imágenes de puño cerrado
-        imgPuño1.src = "../media/img/puño2.png"; // jugador
-        imgPuño2.src = "../media/img/puño.png";  // máquina
-
-
-        // reactivar las animaciones
-        imgPuño1.style.animation = "golpeteo1 0.6s infinite alternate";
-        imgPuño2.style.animation = "golpeteo2 0.6s infinite alternate";
-
-        imgPuño1.style.transform = "scaleX(1)";
-        imgPuño2.style.transform = "scaleX(1)";
-        resultado1.textContent = "";
-    }, 2000);
-
-
+    imgBtnPiedra.style.pointerEvents = "none";
+    imgBtnPapel.style.pointerEvents = "none";
+    imgBtnTijeras.style.pointerEvents = "none";
 
 }
+
+
+
+
 
 
 //evento si el jugador elige papel 
-
 imgBtnPapel.addEventListener("click", okPapel);
 function okPapel() {
     //creamos un contador entre 0 y 2 (0 incluido)
@@ -116,6 +109,7 @@ function okPapel() {
         imgPuño1.style.animation = "none";
         resetPuños()
 
+
     } else if (pptRandom === 2) {
         resultado1.textContent = "Perdiste 😭";
         puntosPerder++;
@@ -126,7 +120,12 @@ function okPapel() {
         imgPuño1.src = "../media/img/tijeras-btn.png";
         imgPuño1.style.transform = "scaleX(-1)";
         imgPuño1.style.animation = "none";
+        winMaquina++;
+
         resetPuños()
+        intentos()
+
+
 
     } else {
 
@@ -137,14 +136,22 @@ function okPapel() {
         imgPuño2.style.transform = "scaleX(-1)"
         imgPuño2.style.animation = "none";
         imgPuño1.style.animation = "none";
+        winJugador++;
         resetPuños();
-
+        intentos()
     }
+
+
+    imgBtnPiedra.style.pointerEvents = "none";
+    imgBtnPapel.style.pointerEvents = "none";
+    imgBtnTijeras.style.pointerEvents = "none";
+
+
 
 }
 
-//evento si el jugador elige tijeras
 
+//evento si el jugador elige tijeras
 imgBtnTijeras.addEventListener("click", okTijeras);
 function okTijeras() {
 
@@ -163,6 +170,7 @@ function okTijeras() {
         imgPuño1.style.transform = "scaleX(-1)"
         resetPuños();
 
+
     } else if (pptRandom === 0) {
         resultado1.textContent = "Perdiste 😭";
         puntosPerder++;
@@ -170,21 +178,147 @@ function okTijeras() {
         imgPuño2.src = "../media/img/tijeras-btn.png";
         imgPuño2.style.animation = "none";
         imgPuño1.style.animation = "none";
+        winMaquina++;
         resetPuños();
+        intentos()
+
+
 
 
     } else {
 
         resultado1.textContent = "Ganaste 😎";
         puntosGanar++;
+
         puntosJugador.textContent = "Puntos del jugador: " + puntosGanar;
         imgPuño2.src = "../media/img/tijeras-btn.png";
         imgPuño2.style.animation = "none";
         imgPuño1.src = "../media/img/papel-btn.png";
         imgPuño1.style.animation = "none";
+        winJugador++;
         resetPuños();
-
+        intentos()
     }
+
+    imgBtnPiedra.style.pointerEvents = "none";
+    imgBtnPapel.style.pointerEvents = "none";
+    imgBtnTijeras.style.pointerEvents = "none";
+
+}
+
+
+
+//WIN/lose
+function intentos() {
+
+
+    if (winJugador === 3) {
+
+        let puños = document.querySelectorAll(".imgPuño1, .imgPuño2");
+        let botones = document.querySelectorAll(".img-btn");
+
+        puños.forEach(puño => puño.classList.add("invisible"));
+        botones.forEach(btn => btn.classList.add("invisible"));
+
+        resultado1.textContent = ""
+
+        winTextJugador.textContent = "Ganaste 😎";
+        winTextJugador.style.color = "black"
+        winTextJugador.style.backgroundColor = "green"
+        winTextJugador.style.padding = "20px"
+        winTextJugador.style.fontSize = "40px"
+        winTextJugador.style.marginTop = "100px"
+
+        let puntos = document.querySelectorAll(".puntosMaquina, .puntosJugador");
+        puntos.forEach(p => p.classList.add("invisible"));
+
+
+
+        let reset = document.querySelectorAll(".resetBtn")
+        reset.forEach(Reset => Reset.classList.remove("d-none"));
+
+
+
+
+
+    } else if (winMaquina === 3) {
+
+        let puños = document.querySelectorAll(".imgPuño1, .imgPuño2");
+        let botones = document.querySelectorAll(".img-btn");
+
+        puños.forEach(puño => puño.classList.add("invisible"));
+        botones.forEach(btn => btn.classList.add("invisible"));
+
+        resultado1.textContent = ""
+
+
+        loseTextJugador.textContent = "Perdiste 😭";
+        loseTextJugador.style.color = "black"
+        loseTextJugador.style.backgroundColor = "red"
+        loseTextJugador.style.padding = "20px"
+        loseTextJugador.style.marginTop = "100px"
+        loseTextJugador.style.fontSize = "40px"
+
+        let puntos = document.querySelectorAll(".puntosMaquina, .puntosJugador");
+        puntos.forEach(p => p.classList.add("invisible"));
+
+
+
+        let reset = document.querySelectorAll(".resetBtn")
+        reset.forEach(Reset => Reset.classList.remove("d-none"));
+
+
+
+
+    } else {
+        //vació
+    }
+
+
+
+}
+
+
+//btn reset
+
+btnReset.addEventListener("click", function () {
+    location.reload(); // recarga la página entera y reinicia el juego
+});
+
+
+
+
+
+
+
+//animación entre turnos
+function resetPuños() {
+    setTimeout(() => {
+        // volver a las imágenes de puño cerrado
+        imgPuño1.src = "../media/img/puño2.png"; // jugador
+        imgPuño2.src = "../media/img/puño.png";  // máquina
+
+
+        // reactivar las animaciones
+        imgPuño1.style.animation = "golpeteo1 0.6s infinite alternate";
+        imgPuño2.style.animation = "golpeteo2 0.6s infinite alternate";
+        //escalas de rotación
+        imgPuño1.style.transform = "scaleX(1)";
+        imgPuño2.style.transform = "scaleX(1)";
+        //desbloquear botones de juego después del ataque 
+        imgBtnPiedra.style.pointerEvents = "auto";
+        imgBtnPapel.style.pointerEvents = "auto";
+        imgBtnTijeras.style.pointerEvents = "auto";
+        resultado1.textContent = "";
+
+
+
+    }, 2000);
+
+
+
+
+
 }
 
 
@@ -199,7 +333,7 @@ btnNext.addEventListener("click", function () {
 })
 
 let btnBackLogin = document.getElementById("btnBackLogin");
-btnBackLogin.addEventListener("click", function() {
+btnBackLogin.addEventListener("click", function () {
 
     window.location.href = "../index.html"
 })
